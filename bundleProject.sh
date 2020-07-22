9d76cad3089e447450bc19d3fa1856e6a065d07c
Jon Graf <jongraf1@gmail.com>
2020-07-22 15:29:24 -0500

    updated deployment script to run tests and zip up projects

--- bundleProject.sh
+++ bundleProject.sh
@@ -1,11 +1,61 @@
+#!/bin/bash
+
+YELLOW='\033[1;33m'
+WHITE='\033[0m'
+GREEN='\033[0;32m'
+
+PRESENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ZIP_DIR=lambda_python
 
 
-function zipFile {
-  VERSION=1.0.0
-  mkdir lambda_test_${VERSION}
-  cp lambda_test.py lambda_test_${VERSION}
-  powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('lambda_test_${VERSION}', 'lambda_test_${VERSION}.zip'); }"
-  rm -rf lambda_test_${VERSION}
+function zipFiles {
+  echo -e "${GREEN}----------Zipping up directory----------${WHITE}"
+  if [[ "$OSTYPE" == "msys" ]]; then
+    echo -e "${YELLOW}----------Windows Powershell Zip----------${WHITE}"
+    powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('${ZIP_DIR}', '${ZIP_DIR}.zip'); }"
+  else
+    echo -e "${YELLOW}----------Linux Bash Zip----------${WHITE}"
+    zip -r ${ZIP_DIR}.zip ${ZIP_DIR}
+  fi
 }
 
-zipFile
+function createDirectories {
+  echo -e "${GREEN}----------Creating zip directory and copying python files----------${WHITE}"
+  mkdir "${ZIP_DIR}"
+  cp app.py ${ZIP_DIR}
+  cp -r src ${ZIP_DIR}
+}
+
+function cleanupDirectory {
+  echo -e "${GREEN}----------Deleting original zip directory----------${WHITE}"
+  rm -rf ${ZIP_DIR}
+}
+
+function installPythonDependencies {
+  echo -e "${GREEN}----------Installing Python files to directory----------${WHITE}"
+  UPDATED_REQUIREMENTS=$(cat requirements.txt | grep -v boto3)
+  UPDATED_REQUIREMENTS=$(echo $UPDATED_REQUIREMENTS | tr '\r\n' ' ')
+  pip install --target="${PRESENT_DIR}/${ZIP_DIR}" -U ${UPDATED_REQUIREMENTS}
+}
+
+function deleteExistingFolders {
+  if [[ -d "$ZIP_DIR" ]]; then
+    echo -e "${GREEN}----------Deleting existing zip directory: ${ZIP_DIR}----------${WHITE}"
+    rm -rf ${ZIP_DIR}
+  fi
+}
+
+function deleteExistingZipFile {
+  if [[ -f "${ZIP_DIR}.zip" ]]; then
+    echo -e "${GREEN}----------Deleting existing zip file: ${ZIP_DIR}.zip----------${WHITE}"
+    rm -rf "${ZIP_DIR}.zip"
+  fi
+}
+
+
+deleteExistingFolders
+deleteExistingZipFile
+createDirectories
+installPythonDependencies
+zipFiles
+cleanupDirectory
